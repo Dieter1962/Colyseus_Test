303e4dbbdf78460a4c60ce13c0f3d2209d5a22a2
Dieter1962 <games@sand-home.de>
2021-09-01 16:28:17 +0200

    Update index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -6,10 +6,19 @@
  * If you're self-hosting (without Arena), you can manually instantiate a
  * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options 
  */
-import { listen } from "@colyseus/arena";
 
-// Import arena config
-import arenaConfig from "./arena.config";
+ gameServer.define('my_room', MyRoom);
 
+
+app.get("/", (req, res) => {
+    res.send("It's time to kick ass and chew bubblegum!");
+});
+
+/**
+ * Bind @colyseus/monitor
+ * It is recommended to protect this route with a password.
+ * Read more: https://docs.colyseus.io/tools/monitor/
+ */
+app.use("/colyseus", monitor());
 // Create and listen on 2567 (or PORT environment variable.)
 listen(arenaConfig);
